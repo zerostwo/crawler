a7082487bdb250f203eb0b33682735447eec9bbc
zerostwo <zerostwo@163.com>
2019-01-19 09:57:35 +0800

    update

--- Face_recognition/AipFace.py
+++ Face_recognition/AipFace.py
@@ -5,9 +5,7 @@ from PIL import Image
 
 cap = cv2.VideoCapture(0)
 while(1):
-    # get a frame
     ret, frame = cap.read()
-    # show a frame
     cv2.imshow("capture", frame)
     if cv2.waitKey(1) & 0xFF == ord('p'):
         cv2.imwrite("/home/duansq/duan/duan.jpg", frame)
